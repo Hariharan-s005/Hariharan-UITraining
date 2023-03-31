1fd9ebc403a26e91022904d8d0f1ba3ff4d9123c
Hariharan-s005 <h.shakthivelrajagopal@cdw.com>
2023-03-31 19:24:07 +0530

    add changes as per review comments

--- js/index.js
+++ js/index.js
@@ -2,30 +2,78 @@
 import videoJsonContents from "/js/video.json" assert { type: "json" };
 import posterJsonContents from "/js/posters.json" assert { type: "json" };
 
+  // creating document fragment for reviewContainer
+let reviewContainerDocumentFragment = document.createDocumentFragment();
+let upcomingProjectsContainer = document.getElementById(
+  "upcomingProjectsContainer"
+);
+//function for apending reviewerContainer elements
+let appendReviewerContainer = () => {
+  for (let comments of videoJsonContents.comments) {
+    //creating html elements with the class name specified
+    let reviewerContainer = document.createElement("div");
+    reviewerContainer.className = "reviewer";
+
+    // appending elements to the container
+    let reviewerImage = document.createElement("img");
+    reviewerImage.src = comments.image;
+    reviewerImage.alt = comments.name;
+    reviewerContainer.append(reviewerImage);
+
+    let reviewerDetails = document.createElement("div");
+    reviewerDetails.className = "reviewer-details";
+    reviewerContainer.append(reviewerDetails);
+
+    let reviewerName = document.createElement("h5");
+    reviewerName.className = "reviewer-name";
+    reviewerName.innerHTML = comments.name;
+    reviewerDetails.append(reviewerName);
+
+    let review = document.createElement("p");
+    review.className = "review";
+    review.innerHTML = comments.comment;
+    reviewerDetails.append(review);
+    reviewContainerDocumentFragment.append(reviewerContainer);
+  }
+};
+  //function for appending upcoming projects
+let appendUpcomingprojects = () => {
+  let upcomingProjectsTitle = document.createElement("h3");
+  upcomingProjectsTitle.innerHTML = "Upcoming Projects";
+  upcomingProjectsContainer.append(upcomingProjectsTitle);
+
+  let upcomingProjectsPosters = document.createElement("div");
+  upcomingProjectsPosters.className = "upcoming-projects-posters";
+
+  let posterImageDocumentFragment = document.createDocumentFragment();
+  for (let poster of posterJsonContents) {
+    let posterPicture = document.createElement("img");
+    posterPicture.alt = poster.title;
+    posterPicture.src = poster.imageUrl;
+    posterImageDocumentFragment.append(posterPicture);
+  }
+  // Appending to upcomingProjectsContainer
+  upcomingProjectsPosters.append(posterImageDocumentFragment);
+  upcomingProjectsContainer.append(upcomingProjectsPosters);
+};
 document.addEventListener("DOMContentLoaded", () => {
   let leftSectionContainer = document.getElementById("leftSectionContainer");
-  let upcomingProjectsContainer = document.getElementById(
-    "upcomingProjectsContainer"
-  );
-
-  // Set play button to the video
-  document.getElementById("play").addEventListener("click", function () {
-    document.getElementById("video").play();
-    document.getElementById("video").setAttribute("controls", "");
-    let playIcon = document.getElementById("play");
-    if (playIcon.style.display == "none") {
-      playIcon.style.display = "block";
-    } else {
-      playIcon.style.display = "none";
-    }
-  });
 
   //creating html elements with the class name specified
+  let videoContainer = document.createElement("div");
+  videoContainer.className = "videoContainer";
   let video = document.createElement("video");
   video.id = "video";
   video.src = videoJsonContents.videoUrl;
   video.poster = videoJsonContents.poster;
-  leftSectionContainer.append(video);
+  videoContainer.append(video);
+
+  let playIcon = document.createElement("img");
+  playIcon.className = "play-icon";
+  playIcon.src = "/img/play-icon.png";
+  videoContainer.append(playIcon);
+
+  leftSectionContainer.append(videoContainer);
 
   let movieDetails = document.createElement("div");
   movieDetails.className = "movie-details";
@@ -54,59 +102,15 @@ document.addEventListener("DOMContentLoaded", () => {
   movieCommentsHeading.innerHTML = "Comments";
   reviewerCommentsContainer.append(movieCommentsHeading);
 
-  // creating document fragment for reviewContainer
-  let reviewContainerDocumentFragment = document.createDocumentFragment();
-
-  //function for apending reviewerContainer elements
-  let appendReviewerContainer = () => {
-    for (let comments of videoJsonContents.comments) {
-      //creating html elements with the class name specified
-      let reviewerContainer = document.createElement("div");
-      reviewerContainer.className = "reviewer";
-
-      // appending elements to the container
-      let reviewerImage = document.createElement("img");
-      reviewerImage.src = comments.image;
-      reviewerImage.alt = comments.name;
-      reviewerContainer.append(reviewerImage);
-
-      let reviewerDetails = document.createElement("div");
-      reviewerDetails.className = "reviewer-details";
-      reviewerContainer.append(reviewerDetails);
-
-      let reviewerName = document.createElement("h5");
-      reviewerName.className = "reviewer-name";
-      reviewerName.innerHTML = comments.name;
-      reviewerDetails.append(reviewerName);
-
-      let review = document.createElement("p");
-      review.className = "review";
-      review.innerHTML = comments.comment;
-      reviewerDetails.append(review);
-      reviewContainerDocumentFragment.append(reviewerContainer);
-    }
-  };
-
-  //function for appending upcoming projects
-  let appendUpcomingprojects = () => {
-    let upcomingProjectsTitle = document.createElement("h3");
-    upcomingProjectsTitle.innerHTML = "Upcoming Projects";
-    upcomingProjectsContainer.append(upcomingProjectsTitle);
-
-    let upcomingProjectsPosters = document.createElement("div");
-    upcomingProjectsPosters.className = "upcoming-projects-posters";
-
-    let posterImageDocumentFragment = document.createDocumentFragment();
-    for (let poster of posterJsonContents) {
-      let posterPicture = document.createElement("img");
-      posterPicture.alt = poster.title;
-      posterPicture.src = poster.imageUrl;
-      posterImageDocumentFragment.append(posterPicture);
-    }
-    // Appending to upcomingProjectsContainer
-    upcomingProjectsPosters.append(posterImageDocumentFragment);
-    upcomingProjectsContainer.append(upcomingProjectsPosters);
-  };
+  // Set play button to the video
+  document
+    .getElementsByClassName("play-icon")[0]
+    .addEventListener("click", function () {
+      document.getElementById("video").play();
+      document.getElementById("video").setAttribute("controls", "");
+      let playIcon = document.getElementsByClassName("play-icon")[0];
+      playIcon.style.display = "none";
+    });
 
   appendReviewerContainer();
 
